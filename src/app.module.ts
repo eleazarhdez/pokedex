563a92a495df816d87a22c4d618d41b17dc44ec2
Eleazar <eleazar87@gmail.com>
2022-03-07 20:45:21 +0000

    Add typeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  imports: [PokemonModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),
+    PokemonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
